47a14438a8ed494921010ddd9b3de16023276f29
khyunah <89136556+khyunah@users.noreply.github.com>
2022-06-17 16:30:58 +0900

    복합키 만드는 연습

---
+++ src/main/java/com/tencoding/blog/model/Movie.java
@@ -0,0 +1,18 @@
+package com.tencoding.blog.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Movie {
+
+	@Id
+	@Column(length = 30)
+	private String titile;
+	@Column(columnDefinition = "TEXT")
+	private String image;
+	@Column(columnDefinition = "DATE")
+	private String releaseDate;
+	private float starScore;
+}
