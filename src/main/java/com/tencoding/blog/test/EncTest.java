b03326863fa352d00b1bcb1e0931ab24cdb24017
GyuDong0920 <yd3085@naver.com>
2022-07-01 11:59:49 +0900

    @Test 오류

--- src/main/java/com/tencoding/blog/test/EncTest.java
+++ src/main/java/com/tencoding/blog/test/EncTest.java
@@ -1,11 +1,11 @@
 package com.tencoding.blog.test;
 
-import org.junit.jupiter.api.Test;
+
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 public class EncTest {
 
-	@Test
+	//@Test
 	public void hashEncode() {
 		String encPassword = new BCryptPasswordEncoder().encode("123");
 		System.out.println("해시 값 : " + encPassword);
